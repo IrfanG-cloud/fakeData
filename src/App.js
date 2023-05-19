5a064efd41f3c1c05708a5480694812ad50aa82f
IrfanGhy <irfanghyann@gmail.com>
2023-05-19 11:27:05 +0500

    updated

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import Notes from './components/Notes/Notes';
+import Content from './shared/layout/Content/Content';
+import Header from './shared/layout/Header/Header';
+import Footer from './shared/layout/Footer/Footer';
+
+// This is our fake data...
+import { notes1, notes2 } from '../src/components/Notes/data';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    // The first time we load the notes1...
+    this.state = {
+      notes: notes1
+    };
+  }
+
+  componentDidMount() {
+    // After 10 seconds (10000 milliseconds) we concatenate our       
+    // data with notes2...
+    setTimeout(() => {
+      this.setState({
+        notes: [...this.state.notes, ...notes2]
+      });
+    }, 10000);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header title="Notes" />
+
+        <Content>
+          <Notes notes={this.state.notes} />
+        </Content>
+
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/shared/layout/Content/Content.js
@@ -0,0 +1,11 @@
+
+function Content() {
+    return (
+      <div >
+        <h1> Content here</h1>
+      </div>
+    );
+  }
+  
+  export default Content;
+  
